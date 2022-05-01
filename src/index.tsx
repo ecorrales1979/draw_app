357665a851d060c3612a713a23819af3c9ca3f98
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-05-01 14:38:36 -0300

    feat: delete unused files

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import { Provider as ReduxProvider } from 'react-redux'
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { store } from './stores';
 
 const root = ReactDOM.createRoot(
@@ -17,8 +16,3 @@ root.render(
     </ReduxProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
